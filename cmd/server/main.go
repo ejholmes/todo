f8edbe44245d71f027f3948b176dab928cca078c
Eric J. Holmes <eric@ejholmes.net>
2014-09-12 09:03:49 -0700

    Port option.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 
 	"github.com/remind101/todo"
 )
 
 func main() {
+	var port = flag.String("port", "3000", "The port to run the server on.")
+
 	c := todo.New()
 	s := todo.NewServer(c)
 
-	http.ListenAndServe(":3000", s)
+	http.ListenAndServe(":"+*port, s)
 }
