b274d0db429c072d6d8a8854dc81c74f8025e648
Eric J. Holmes <eric@ejholmes.net>
2014-09-12 09:14:43 -0700

    Correct.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 
-	"github.com/remind101/todo"
+	"github.com/ejholmes/todo"
 )
 
 func main() {
 	var port = flag.String("port", "3000", "The port to run the server on.")
+	flag.Parse()
 
 	c := todo.New()
 	s := todo.NewServer(c)
 
+	fmt.Printf("Listening on %s\n", *port)
 	http.ListenAndServe(":"+*port, s)
 }
