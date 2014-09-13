607a127bec730f64b07271eb13316896760360bc
Eric J. Holmes <eric@ejholmes.net>
2014-09-12 22:04:54 -0700

    Allow DELETE.

--- server.go
+++ server.go
@@ -87,7 +87,7 @@ func (s *Server) Handle(method, path string, fn HandlerFunc) {
 	s.mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, PATCH, GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, PATCH, GET, DELETE, OPTIONS")
 		fn(s.Client, &ResponseWriter{w}, &Request{r})
 	}).Methods(method)
 }
