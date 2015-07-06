fd5c93323255a5f215af951dfd3c066244a4591d
Eric J. Holmes <eric@ejholmes.net>
2015-07-06 16:27:50 -0700

    Use PORT environment variable if present.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,12 +4,13 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/ejholmes/todo"
 )
 
 func main() {
-	var port = flag.String("port", "3000", "The port to run the server on.")
+	var port = flag.String("port", stringVar("PORT", "3000"), "The port to run the server on.")
 	flag.Parse()
 
 	c := todo.New()
@@ -17,4 +18,13 @@ func main() {
 
 	log.Printf("Listening on %s\n", *port)
 	log.Fatal(http.ListenAndServe(":"+*port, s))
+}
+
+func stringVar(key, fb string) string {
+	v := os.Getenv(key)
+	if v != "" {
+		return v
+	}
+
+	return fb
 }
